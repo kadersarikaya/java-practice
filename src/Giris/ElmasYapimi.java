879b856b8a166364bb89545e5e6a79dd7998a72b
Kader Sarıkaya <kadersarikaya0123@gmail.com>
2024-01-16 11:51:35 +0300

    elmas

--- src/Giris/ElmasYapimi.java
+++ src/Giris/ElmasYapimi.java
@@ -1,2 +1,42 @@
-package Giris;public class ElmasYapimi {
+package Giris;
+import java.util.Scanner;
+
+public class ElmasYapimi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Elmasın yüksekliğini giriniz: ");
+        int yukseklik = scanner.nextInt();
+
+        if (yukseklik % 2 == 0) {
+            System.out.println("Hatalı giriş! Yükseklik tek olmalıdır.");
+            return;
+        }
+
+        int yarimYukseklik = yukseklik / 2;
+
+        // Üst kısmı oluşturma
+        for (int i = 0; i <= yarimYukseklik; i++) {
+            for (int j = 0; j < yarimYukseklik - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Alt kısmı oluşturma
+        for (int i = yarimYukseklik - 1; i >= 0; i--) {
+            for (int j = 0; j < yarimYukseklik - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
 }
