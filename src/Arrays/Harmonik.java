f9ac883a058f9e9a11a4dddb81c0af94b3cef695
Kader Sarıkaya <kadersarikaya0123@gmail.com>
2024-01-29 21:32:52 +0300

    harmonikdizi

--- src/Arrays/Harmonik.java
+++ src/Arrays/Harmonik.java
@@ -1,12 +1,39 @@
 package Arrays;
+import java.util.Scanner;
 
 public class Harmonik {
     public static void main(String[] args) {
-        double[] numbers = {1, 2, 3, 4, 5};
-        double sum = 0;
-        for(int i=0; i< numbers.length; i++) {
-            sum+=1/numbers[i];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dizi boyutunu giriniz: ");
+        int n = scanner.nextInt();
+
+        // Dizi oluşturma
+        double[] dizi = new double[n];
+
+        // Dizi elemanlarını kullanıcıdan alma
+        for (int i = 0; i < n; i++) {
+            System.out.print("Dizinin " + (i + 1) + ". elemanını giriniz: ");
+            dizi[i] = scanner.nextDouble();
         }
-        System.out.println("result : " + sum);
+
+        // Harmonik ortalama hesaplama
+        double harmonikOrtalama = hesaplaHarmonikOrtalama(dizi);
+
+        // Sonucu ekrana yazdırma
+        System.out.println("Dizinin harmonik ortalaması: " + harmonikOrtalama);
+    }
+
+    // Harmonik ortalama hesaplayan fonksiyon
+    private static double hesaplaHarmonikOrtalama(double[] dizi) {
+        double toplam = 0;
+
+        // Harmonik seriyi hesaplama
+        for (int i = 0; i < dizi.length; i++) {
+            toplam += 1 / dizi[i];
+        }
+
+        // Harmonik ortalama hesaplama
+        return dizi.length / toplam;
     }
 }
